961ac1725a859f1a19534a4418a5711c9ff3164c
ecandotti <enzo.candotti16@hotmail.com>
2021-04-05 15:25:34 +0200

    update

--- migrations/Version20210405131841.php
+++ migrations/Version20210405131841.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210325012413 extends AbstractMigration
+final class Version20210405131841 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,10 +23,12 @@ final class Version20210325012413 extends AbstractMigration
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, content VARCHAR(255) DEFAULT \'W\' NOT NULL, status VARCHAR(5) NOT NULL, create_at DATETIME NOT NULL, INDEX IDX_9474526CA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE gallery (id INT AUTO_INCREMENT NOT NULL, content LONGTEXT NOT NULL, create_at DATETIME NOT NULL, status VARCHAR(5) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE paypal_express (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE pronostic (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, create_at VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, title VARCHAR(255) NOT NULL, create_date DATETIME NOT NULL, result VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, INDEX IDX_E64BDCDEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pronostic (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, content LONGTEXT NOT NULL, title VARCHAR(255) NOT NULL, result VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, create_at DATETIME NOT NULL, publish_at DATETIME NOT NULL, match_name VARCHAR(255) NOT NULL, parier VARCHAR(255) NOT NULL, cote VARCHAR(255) NOT NULL, heure DATETIME NOT NULL, INDEX IDX_E64BDCDEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, pseudo VARCHAR(255) NOT NULL, create_at DATETIME NOT NULL, start_vip DATETIME DEFAULT NULL, end_vip DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE pronostic ADD CONSTRAINT FK_E64BDCDEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema) : void
@@ -34,10 +36,12 @@ final class Version20210325012413 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
         $this->addSql('ALTER TABLE pronostic DROP FOREIGN KEY FK_E64BDCDEA76ED395');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE gallery');
         $this->addSql('DROP TABLE paypal_express');
         $this->addSql('DROP TABLE pronostic');
+        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE user');
     }
 }
